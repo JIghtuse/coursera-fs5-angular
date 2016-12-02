9ef94b93459dd284d3aa33ee15ab14e2c1901dfb
Boris Egorov <egorov@linux.com>
2016-12-02 23:26:22 +0700

    digest_and_apply: Use Angular-specific service $timeout

--- digest_and_apply/js/app.js
+++ digest_and_apply/js/app.js
@@ -4,16 +4,14 @@
 angular.module("CounterApp", [])
 .controller("CounterController", CounterController);
 
-CounterController.$inject = ['$scope'];
-function CounterController($scope) {
+CounterController.$inject = ['$scope', '$timeout'];
+function CounterController($scope, $timeout) {
     $scope.counter = 0;
 
     $scope.upCounter = function() {
-        setTimeout(function() {
-            $scope.$apply(function() {
-                $scope.counter++;
-                console.log("Counter incremented");
-            });
+        $timeout(function() {
+            $scope.counter++;
+            console.log("Counter incremented");
         }, 2000);
     };
 }
