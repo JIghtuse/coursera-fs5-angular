ddd29d161bbb3d59e161047324484deb3f64af15
Boris Egorov <egorov@linux.com>
2016-12-06 22:04:35 +0700

    shopping_list_check_off: Initial solution

---
+++ shopping_list_check_off/js/app.js
@@ -0,0 +1,64 @@
+(function() {
+
+angular.module("ShoppingListCheckOff", [])
+.controller("ToBuyController", ToBuyController)
+.controller("AlreadyBoughtController", AlreadyBoughtController)
+.service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toByList = this;
+  toByList.items = ShoppingListCheckOffService.getToByItems();
+
+  toByList.buy = function(index) {
+    ShoppingListCheckOffService.buy(index);
+  }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  var toByItems = [
+    {
+      name: "cookies",
+      quantity: 5
+    },
+    {
+      name: "chocolate",
+      quantity: 12
+    },
+    {
+      name: "marshmallow",
+      quantity: 2
+    },
+    {
+      name: "Nutella",
+      quantity: 14
+    }
+  ];
+
+  var boughtItems = [];
+
+  service.getToByItems = function() {
+    return toByItems;
+  }
+
+  service.getBoughtItems = function() {
+    return boughtItems;
+  }
+
+  service.buy = function(index) {
+    var item = toByItems[index];
+    toByItems.splice(index, 1);
+    boughtItems.push(item);
+    console.log(`Bought item ${index}: ${item}`);
+  }
+}
+
+})();
